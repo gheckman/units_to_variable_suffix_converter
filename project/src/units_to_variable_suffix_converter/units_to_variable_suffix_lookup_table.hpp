eb02e4127e1a67eacebf047bdf74ac39a0c5da6d
Trevor Hickey <thickey@f2si.net>
2015-12-30 11:28:09 -0500

    updates

--- project/src/units_to_variable_suffix_converter/units_to_variable_suffix_lookup_table.hpp
+++ project/src/units_to_variable_suffix_converter/units_to_variable_suffix_lookup_table.hpp
@@ -86,6 +86,7 @@ static const std::unordered_multimap<std::string,std::string> UNITS_TO_VARIABLE_
   ,{"centuries","cents"}
   ,{"millennium","millen"}
   ,{"eons","eons"}
+  ,{"ticks","ticks"}
   
   //temperature
   ,{"degree farenheit","degf"}
